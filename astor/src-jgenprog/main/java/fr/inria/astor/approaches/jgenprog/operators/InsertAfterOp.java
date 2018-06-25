c8b696b8da237f99762761849a3970100ac754f6
Kui Liu <brucekuiliu@gmail.com>
2018-06-25 19:05:30 +0800

    astor

---
+++ astor/src-jgenprog/main/java/fr/inria/astor/approaches/jgenprog/operators/InsertAfterOp.java
@@ -0,0 +1,63 @@
+package fr.inria.astor.approaches.jgenprog.operators;
+
+import fr.inria.astor.core.entities.ModificationPoint;
+import fr.inria.astor.core.entities.OperatorInstance;
+import fr.inria.astor.core.entities.ProgramVariant;
+import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtReturn;
+import spoon.reflect.code.CtStatement;
+
+/**
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class InsertAfterOp extends InsertOp {
+
+	@Override
+	public boolean applyChangesInModel(OperatorInstance operation, ProgramVariant p) {
+		boolean successful = false;
+		CtStatement ctst = (CtStatement) operation.getOriginal();
+		CtStatement fix = (CtStatement) operation.getModified();
+
+		CtBlock parentBlock = operation.getParentBlock();
+
+		if (parentBlock != null) {
+			ctst.insertAfter((CtStatement) fix);
+			fix.setParent(parentBlock);
+			successful = true;
+			operation.setSuccessfulyApplied(successful);
+			this.updateBlockImplicitly(parentBlock, true);
+		} else {
+			log.error("Operation not applied. Parent null");
+		}
+		return successful;
+	}
+
+	@Override
+	public boolean undoChangesInModel(OperatorInstance operation, ProgramVariant p) {
+		CtStatement fix = (CtStatement) operation.getModified();
+		CtBlock<?> parentBlock = operation.getParentBlock();
+		int position = operation.getLocationInParent();
+		position += 1;
+		boolean sucessful = remove(parentBlock, fix, position);
+		parentBlock.setImplicit(operation.isParentBlockImplicit());
+		return sucessful;
+	}
+
+	@Override
+	public boolean canBeAppliedToPoint(ModificationPoint point) {
+
+		boolean apply = super.canBeAppliedToPoint(point);
+		if (!apply)
+			return apply;
+
+		// do not insert after a return
+		if (point.getCodeElement() instanceof CtReturn) {
+			return false;
+		}
+
+		// Otherwise, accept the element
+		return true;
+	}
+}
---
+++ astor/src-jgenprog/main/java/fr/inria/astor/approaches/jgenprog/operators/InsertBeforeOp.java
@@ -0,0 +1,65 @@
+package fr.inria.astor.approaches.jgenprog.operators;
+
+import fr.inria.astor.core.entities.ModificationPoint;
+import fr.inria.astor.core.entities.OperatorInstance;
+import fr.inria.astor.core.entities.ProgramVariant;
+import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtConstructorCall;
+import spoon.reflect.code.CtStatement;
+/**
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class InsertBeforeOp extends InsertOp {
+
+	
+	@Override
+	public boolean applyChangesInModel(OperatorInstance operation, ProgramVariant p) {
+		boolean successful = false;
+		CtStatement ctst = (CtStatement) operation.getOriginal();
+		CtStatement fix = (CtStatement) operation.getModified();
+
+		CtBlock parentBlock = operation.getParentBlock();
+
+		if (parentBlock != null) {
+			ctst.insertBefore((CtStatement) fix);
+			fix.setParent(parentBlock);
+			successful = true;
+			operation.setSuccessfulyApplied(successful);
+			this.updateBlockImplicitly(parentBlock, true);
+		} else {
+			log.error("Operation not applied. Parent null ");
+		}
+		return successful;
+	
+	}
+	@Override
+	public boolean undoChangesInModel(OperatorInstance operation, ProgramVariant p) {
+		CtStatement ctst = (CtStatement) operation.getOriginal();
+		CtStatement fix = (CtStatement) operation.getModified();
+		CtBlock<?> parentBlock = operation.getParentBlock();
+		int position = operation.getLocationInParent();
+		boolean sucess = remove(parentBlock, fix, position);
+		parentBlock.setImplicit(operation.isParentBlockImplicit());
+		return sucess;
+
+	}
+	
+	@Override
+	public boolean canBeAppliedToPoint(ModificationPoint point) {
+
+		boolean apply = super.canBeAppliedToPoint(point);
+		if (!apply)
+			return apply;
+
+		// do not insert after a return
+		if (point.getCodeElement() instanceof CtConstructorCall) {
+			return false;
+		}
+
+		// Otherwise, accept the element
+		return true;
+	}
+
+}
